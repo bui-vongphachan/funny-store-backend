574c91b784ddd2010a3717d60c201e1d964ee5ce
vongphachan~ <vongphachannukkalash@gmail.com>
2024-02-12 22:33:01 +0700

    Add CURRENT_PROXY environment variable and set trusted proxies in router

--- routes/index.go
+++ routes/index.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -8,6 +10,10 @@ import (
 func SetupRouter(db *mongo.Database) *gin.Engine {
 
 	router := gin.Default()
+
+	CURRENT_PROXY := os.Getenv("CURRENT_PROXY")
+
+	router.SetTrustedProxies([]string{CURRENT_PROXY})
 
 	// create_user_routes.CreateUser(db, router)
 	// company_routes.CreateCompany(db, router)
