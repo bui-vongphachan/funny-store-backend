23831e1ec1fa39f97597bf05ce8adf8ffb92c2dc
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-06 02:36:34 +0700

    Add CreateEmpty function to product service

---
+++ src/services/product/index.go
@@ -0,0 +1,22 @@
+package serviceproductdraft
+
+import (
+	model "github.com/vongphachan/funny-store-backend/src/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func CreateEmpty() model.Product {
+	output := model.Product{
+		ID:                  primitive.NewObjectID().Hex(),
+		Title:               "",
+		Description:         "",
+		PreviewImages:       []string{},
+		Gallery:             []string{},
+		Delete:              false,
+		HavingSingleVariant: false,
+		Image:               "",
+		IsDraft:             true,
+	}
+
+	return output
+}
