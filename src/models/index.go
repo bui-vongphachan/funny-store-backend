ff7603ef80ac128265b604bb1094c88095a1275d
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-06 09:52:59 +0700

    Update Product struct in index.go

--- src/models/index.go
+++ src/models/index.go
@@ -1,13 +1,15 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Product struct {
-	ID                  string   `json:"_id"`
-	Title               string   `json:"title"`
-	Description         string   `json:"description"`
-	PreviewImages       []string `json:"previewImages"`
-	Gallery             []string `json:"gallery"`
-	Delete              bool     `json:"delete"`
-	HavingSingleVariant bool     `json:"havingSingleVariant"`
-	Image               string   `json:"image,omitempty"`
-	IsDraft             bool     `json:"isDraft"`
+	ID                  primitive.ObjectID `json:"_id"`
+	Title               string             `json:"title"`
+	Description         string             `json:"description"`
+	PreviewImages       []string           `json:"previewImages"`
+	Gallery             []string           `json:"gallery"`
+	Delete              bool               `json:"delete"`
+	HavingSingleVariant bool               `json:"havingSingleVariant"`
+	Image               string             `json:"image,omitempty"`
+	IsDraft             bool               `json:"isDraft"`
 }
