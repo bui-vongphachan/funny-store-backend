8bf33f9522fd9db5925090fc7c3bbc4a505efe5a
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-09 03:31:20 +0700

    Refactor main.go to use initiators package

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"github.com/vongphachan/funny-store-backend/src/initiators"
 	initiatesmongodb "github.com/vongphachan/funny-store-backend/src/initiators/mongodb"
-	"github.com/vongphachan/funny-store-backend/src/routes"
 )
 
 func main() {
 	db := initiatesmongodb.Start()
 
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-
-	GIN_MODE := os.Getenv("GIN_MODE")
-
-	if GIN_MODE == "release" {
-		gin.SetMode(gin.ReleaseMode)
-	} else {
-		gin.SetMode(gin.DebugMode)
-	}
-
-	r := routes.SetupRouter(db)
-
-	PORT := os.Getenv("PORT")
-	if PORT == "" {
-		PORT = "8080"
-	}
-
-	log.Print("Server is running on port " + PORT + "...")
-
-	r.Run("localhost:" + PORT)
+	initiators.StartGin(db)
 }
