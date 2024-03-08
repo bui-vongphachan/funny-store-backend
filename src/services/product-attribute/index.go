7a2b394692301246563c12cb73851a2d3e81bbdc
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-08 11:51:19 +0700

    Add Save and SaveBulk functions to product-attribute service

--- src/services/product-attribute/index.go
+++ src/services/product-attribute/index.go
@@ -1,9 +1,14 @@
 package serviceproductattribute
 
 import (
+	"context"
+	"log"
+
+	collectionname "github.com/vongphachan/funny-store-backend/src/constraints/table-names"
 	model "github.com/vongphachan/funny-store-backend/src/models"
 	modelproduct "github.com/vongphachan/funny-store-backend/src/models/products"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type CreateEmptyProps struct {
@@ -22,4 +27,31 @@ func CreateEmpty(props *CreateEmptyProps) *modelproduct.Attribute {
 	}
 
 	return &output
+}
+
+func Save(db *mongo.Database, payload *modelproduct.Attribute) *modelproduct.Attribute {
+
+	context := context.TODO()
+
+	_, err := db.Collection(collectionname.PRODUCT_ATTRIBUTE).InsertOne(context, payload)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+		return nil
+	}
+
+	return payload
+}
+
+func SaveBulk(db *mongo.Database, payload *[]interface{}) *[]interface{} {
+	context := context.TODO()
+
+	_, err := db.Collection(collectionname.PRODUCT_ATTRIBUTE).InsertMany(context, *payload)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+		return nil
+	}
+
+	return payload
 }
