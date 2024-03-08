67e88727ecbfa5570894dcc421937219a301ea85
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-08 11:31:53 +0700

    Add SaveToDatabase function to save product to database

--- src/services/product/index.go
+++ src/services/product/index.go
@@ -1,8 +1,14 @@
 package serviceproduct
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	collectionname "github.com/vongphachan/funny-store-backend/src/constraints/table-names"
 	model "github.com/vongphachan/funny-store-backend/src/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func CreateEmpty() *model.Product {
@@ -19,4 +25,30 @@ func CreateEmpty() *model.Product {
 	}
 
 	return &output
+}
+
+type SaveToDatabaseProps struct {
+	DB      *mongo.Database
+	Product *model.Product
+}
+
+func SaveToDatabase(props *SaveToDatabaseProps) *model.Product {
+
+	context := context.TODO()
+
+	cursur, err := props.DB.Collection(collectionname.PRODUCT).InsertOne(context, props.Product)
+
+	if err != nil {
+		return nil
+	}
+
+	jsonData, err := json.MarshalIndent(cursur, "", "    ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", jsonData)
+
+	return props.Product
 }
