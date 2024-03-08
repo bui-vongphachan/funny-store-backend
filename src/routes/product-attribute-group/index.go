e64b601ef92c32f73f2792c882edac7b23dd3e8b
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-08 20:09:21 +0700

    Add update functionality for product attribute group

--- src/routes/product-attribute-group/index.go
+++ src/routes/product-attribute-group/index.go
@@ -1,10 +1,14 @@
 package routeproduct
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	collectionname "github.com/vongphachan/funny-store-backend/src/constraints/table-names"
+	modelproduct "github.com/vongphachan/funny-store-backend/src/models/products"
 	serviceproductattributegroup "github.com/vongphachan/funny-store-backend/src/services/product-attribute-group"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -36,6 +40,56 @@ func Create(db *mongo.Database, r *gin.Engine) {
 		serviceproductattributegroup.Save(db, attributeGroup)
 
 		result["data"] = attributeGroup
+
+		c.JSON(http.StatusOK, result)
+	})
+
+}
+
+func Update(db *mongo.Database, r *gin.Engine) {
+	r.PATCH("/product/attribute-group/:id", func(c *gin.Context) {
+		result := gin.H{
+			"status":  400,
+			"isError": true,
+			"data":    nil,
+			"message": "ຂໍ້ມູນບໍ່ຖືກຕ້ອງ",
+		}
+
+		attributeGroupId := c.Param("id")
+
+		attributeGroup := serviceproductattributegroup.FindById(db, &attributeGroupId)
+		if attributeGroup == nil {
+			result["data"] = nil
+			result["isError"] = true
+			result["status"] = 404
+			result["message"] = "Data not found"
+
+			c.JSON(http.StatusOK, result)
+
+			return
+		}
+
+		var requestBody modelproduct.AttributeGroup
+
+		err := c.Bind(&requestBody)
+		if err != nil {
+			result["message"] = err.Error()
+			c.JSON(http.StatusOK, result)
+		}
+
+		id, err := primitive.ObjectIDFromHex(c.Param("id"))
+		if err != nil {
+			result["message"] = err.Error()
+			c.JSON(http.StatusOK, result)
+		}
+
+		filter := bson.M{"_id": id}
+
+		update := bson.M{"$set": bson.M{"avg_rating": 4}}
+
+		context := context.TODO()
+
+		db.Collection(collectionname.PRODUCT_ATTRIBUTE_GROUPS).UpdateOne(context, filter, bson.M{"$set": update})
 
 		c.JSON(http.StatusOK, result)
 	})
--- src/services/product-attribute-group/index.go
+++ src/services/product-attribute-group/index.go
@@ -6,6 +6,7 @@ import (
 
 	collectionname "github.com/vongphachan/funny-store-backend/src/constraints/table-names"
 	modelproduct "github.com/vongphachan/funny-store-backend/src/models/products"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -35,3 +36,49 @@ func Save(db *mongo.Database, attributeGroup *modelproduct.AttributeGroup) *mode
 
 	return attributeGroup
 }
+
+func FindById(db *mongo.Database, id *string) *modelproduct.AttributeGroup {
+	context := context.TODO()
+
+	objectID, err := primitive.ObjectIDFromHex(*id)
+	if err != nil {
+		log.Fatalln(err.Error())
+		return nil
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	result := db.Collection(collectionname.PRODUCT_ATTRIBUTE_GROUPS).FindOne(context, filter)
+	if result.Err() != nil {
+		log.Fatalln(result.Err().Error())
+		return nil
+	}
+
+	var attributeGroup modelproduct.AttributeGroup
+	err = result.Decode(&attributeGroup)
+	if err != nil {
+		log.Fatalln(err.Error())
+		return nil
+	}
+
+	return &attributeGroup
+}
+
+// func ExcludeEmptyData(payload *modelproduct.AttributeGroup) *modelproduct.AttributeGroup {
+// 	data := make(map[string]interface{})
+
+// 	if payload.Title != "" {
+// 		data["Title"] = payload.Title
+// 	}
+
+// 	output := &modelproduct.AttributeGroup{}
+// }
+
+// func Update(db *mongo.Database, id *string) {
+// 	filter := bson.M{"_id": id}
+
+// 	context := context.TODO()
+
+// 	db.Collection(collectionname.PRODUCT_ATTRIBUTE_GROUPS).UpdateOne(context, filter, bson.M{"$set": update})
+
+// }
