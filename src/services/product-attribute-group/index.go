f4919d17672aa17d8238e6d568038cb92e576b40
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-08 11:40:01 +0700

    Add Save function to save attribute group to database

--- src/services/product-attribute-group/index.go
+++ src/services/product-attribute-group/index.go
@@ -1,8 +1,13 @@
 package serviceproductattributegroup
 
 import (
+	"context"
+	"log"
+
+	collectionname "github.com/vongphachan/funny-store-backend/src/constraints/table-names"
 	modelproduct "github.com/vongphachan/funny-store-backend/src/models/products"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func CreateEmpty(productId *primitive.ObjectID) *modelproduct.AttributeGroup {
@@ -15,4 +20,18 @@ func CreateEmpty(productId *primitive.ObjectID) *modelproduct.AttributeGroup {
 	}
 
 	return &output
+}
+
+func Save(db *mongo.Database, attributeGroup *modelproduct.AttributeGroup) *modelproduct.AttributeGroup {
+
+	context := context.TODO()
+
+	_, err := db.Collection(collectionname.PRODUCT_ATTRIBUTE_GROUPS).InsertOne(context, attributeGroup)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+		return nil
+	}
+
+	return attributeGroup
 }
