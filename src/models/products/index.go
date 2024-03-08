3d680232e9a294b2f1d6c441d4f7b3e2933a460a
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-08 11:33:04 +0700

    Refactor model structs to use bson tags

--- src/models/products/index.go
+++ src/models/products/index.go
@@ -3,38 +3,38 @@ package modelproduct
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AttributeGroup struct {
-	ID        primitive.ObjectID `json:"_id"`
-	Title     string             `json:"title"`
-	IsPrimary bool               `json:"isPrimary"`
-	ProductID primitive.ObjectID `json:"productId"`
-	Delete    bool               `json:"delete"`
+	ID        primitive.ObjectID `json:"_id" bson:"_id"`
+	Title     string             `json:"title" bson:"title"`
+	IsPrimary bool               `json:"isPrimary" bson:"isPrimary"`
+	ProductID primitive.ObjectID `json:"productId" bson:"productId"`
+	Delete    bool               `json:"delete" bson:"delete"`
 }
 
 type Attribute struct {
-	ID                primitive.ObjectID  `json:"_id"`
-	Title             string              `json:"title"`
-	Image             string              `json:"image,omitempty"`
-	ProductID         primitive.ObjectID  `json:"productId"`
-	AttributeGroupID  primitive.ObjectID  `json:"attributeGroupId"`
-	Delete            bool                `json:"delete"`
-	CloudflareImageID string              `json:"cloudflareImageId,omitempty"`
-	ReuseFrom         *primitive.ObjectID `json:"reuseFrom,omitempty"`
+	ID                primitive.ObjectID  `json:"_id" bson:"_id"`
+	Title             string              `json:"title" bson:"title"`
+	Image             string              `json:"image,omitempty" bson:"image,omitempty"`
+	ProductID         primitive.ObjectID  `json:"productId" bson:"productId"`
+	AttributeGroupID  primitive.ObjectID  `json:"attributeGroupId" bson:"attributeGroupId"`
+	Delete            bool                `json:"delete" bson:"delete"`
+	CloudflareImageID string              `json:"cloudflareImageId,omitempty" bson:"cloudflareImageId,omitempty"`
+	ReuseFrom         *primitive.ObjectID `json:"reuseFrom,omitempty" bson:"reuseFrom,omitempty"`
 }
 
 type Variation struct {
-	ID              primitive.ObjectID  `json:"_id"`
-	ProductID       primitive.ObjectID  `json:"productId"`
-	Price           float64             `json:"price"`
-	Delete          bool                `json:"delete"`
-	Stock           int                 `json:"stock"`
-	IsSingleVariant bool                `json:"isSingleVariant"`
-	IsReady         bool                `json:"isReady"`
-	ReuseForm       *primitive.ObjectID `json:"reuseForm,omitempty"`
+	ID              primitive.ObjectID  `json:"_id" bson:"_id"`
+	ProductID       primitive.ObjectID  `json:"productId" bson:"productId"`
+	Price           float64             `json:"price" bson:"price"`
+	Delete          bool                `json:"delete" bson:"delete"`
+	Stock           int                 `json:"stock" bson:"stock"`
+	IsSingleVariant bool                `json:"isSingleVariant" bson:"isSingleVariant"`
+	IsReady         bool                `json:"isReady" bson:"isReady"`
+	ReuseForm       *primitive.ObjectID `json:"reuseForm,omitempty" bson:"reuseForm,omitempty"`
 }
 
 type VariationAttribute struct {
-	ID               primitive.ObjectID `json:"_id"`
-	VariationID      primitive.ObjectID `json:"variationId"`
-	AttributeID      primitive.ObjectID `json:"attributeId"`
-	AttributeGroupID primitive.ObjectID `json:"attributeGroupId"`
+	ID               primitive.ObjectID `json:"_id" bson:"_id"`
+	VariationID      primitive.ObjectID `json:"variationId" bson:"variationId"`
+	AttributeID      primitive.ObjectID `json:"attributeId" bson:"attributeId"`
+	AttributeGroupID primitive.ObjectID `json:"attributeGroupId" bson:"attributeGroupId"`
 }
