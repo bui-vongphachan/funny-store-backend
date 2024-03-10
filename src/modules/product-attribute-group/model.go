7878789b7d18aa7967127db2c15123eba31612ce
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-10 23:38:38 +0700

    Added original id of product attribute group

--- src/modules/product-attribute-group/model.go
+++ src/modules/product-attribute-group/model.go
@@ -3,11 +3,12 @@ package product_attribute_group
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AttributeGroup struct {
-	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	Title     string             `json:"title" bson:"title"`
-	IsPrimary bool               `json:"isPrimary" bson:"isPrimary"`
-	ProductID primitive.ObjectID `json:"productId" bson:"productId"`
-	Delete    bool               `json:"delete" bson:"delete"`
+	ID         primitive.ObjectID `json:"_id" bson:"_id"`
+	Title      string             `json:"title" bson:"title"`
+	IsPrimary  bool               `json:"isPrimary" bson:"isPrimary"`
+	ProductID  primitive.ObjectID `json:"productId" bson:"productId"`
+	Delete     bool               `json:"delete" bson:"delete"`
+	OriginalID primitive.ObjectID `json:"originalId" bson:"originalId"`
 }
 
 type PaginationQuery struct {
@@ -15,9 +16,10 @@ type PaginationQuery struct {
 	Skip  int `json:"skip"`
 
 	// extends base type
-	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	Title     string             `json:"title" bson:"title"`
-	IsPrimary bool               `json:"isPrimary" bson:"isPrimary"`
-	ProductID primitive.ObjectID `json:"productId" bson:"productId"`
-	Delete    bool               `json:"delete" bson:"delete"`
+	ID         primitive.ObjectID `json:"_id" bson:"_id"`
+	Title      string             `json:"title" bson:"title"`
+	IsPrimary  bool               `json:"isPrimary" bson:"isPrimary"`
+	ProductID  primitive.ObjectID `json:"productId" bson:"productId"`
+	Delete     bool               `json:"delete" bson:"delete"`
+	OriginalID primitive.ObjectID `json:"originalId" bson:"originalId"`
 }
