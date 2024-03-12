7bd050b145e1cf9c59f672ebb16ae0fb4c948afc
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-13 02:03:16 +0700

    Add replication functionality for product variation attributes

--- src/modules/product-variations-attributes/_test/replicate_test.go
+++ src/modules/product-variations-attributes/_test/replicate_test.go
@@ -1,37 +1,101 @@
 package product_variations_attributes_test
 
 import (
-	"reflect"
+	"log"
 	"testing"
 
+	product_attribute_group "github.com/vongphachan/funny-store-backend/src/modules/product-attribute-group"
+	product_attribute "github.com/vongphachan/funny-store-backend/src/modules/product-attributes"
+	product_variations "github.com/vongphachan/funny-store-backend/src/modules/product-variations"
 	product_variations_attributes "github.com/vongphachan/funny-store-backend/src/modules/product-variations-attributes"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-
 func TestReplicate(t *testing.T) {
-	type args struct {
-		props *product_variations_attributes.Props_Replicate
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *[]product_variations_attributes.ProductVariationAttribute
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := Replicate(tt.args.props)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Replicate() error = %v, wantErr %v", err, tt.wantErr)
-				return
+	sourceProductId := primitive.NewObjectID()
+	sourceAttributes := []product_attribute.ProductAttribute{}
+	sourceAttributeGroups := []product_attribute_group.AttributeGroup{}
+	sourceVariations := []product_variations.ProductVariation{}
+	sourceVariationAttributes := []product_variations_attributes.ProductVariationAttribute{}
+
+	{
+		for i := 0; i < 2; i++ {
+			attributeGroup := product_attribute_group.AttributeGroup{
+				ID:        primitive.NewObjectID(),
+				ProductID: sourceProductId,
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Replicate() = %v, want %v", got, tt.want)
+
+			sourceAttributeGroups = append(sourceAttributeGroups, attributeGroup)
+		}
+
+		for i := 0; i < 2; i++ {
+			attribute := product_attribute.ProductAttribute{
+				ID:               primitive.NewObjectID(),
+				ProductID:        sourceProductId,
+				AttributeGroupID: sourceAttributeGroups[i].ID,
 			}
-		})
+
+			sourceAttributes = append(sourceAttributes, attribute)
+		}
+
+		for i := 0; i < 2; i++ {
+			variation := product_variations.ProductVariation{
+				ID:        primitive.NewObjectID(),
+				ProductID: sourceProductId,
+			}
+
+			sourceVariations = append(sourceVariations, variation)
+		}
+
+		for i := 0; i < 2; i++ {
+			variationAttribute := product_variations_attributes.ProductVariationAttribute{
+				ID:               primitive.NewObjectID(),
+				AttributeGroupId: sourceAttributeGroups[i].ID,
+				VariationId:      sourceVariations[i].ID,
+				AttributeId:      sourceAttributes[i].ID,
+				ProductId:        sourceProductId,
+			}
+
+			sourceVariationAttributes = append(sourceVariationAttributes, variationAttribute)
+		}
 	}
+
+	newProductID := primitive.NewObjectID()
+	newAttributes := product_attribute.Replicate(&newProductID, &sourceAttributes)
+	newVariations := product_variations.Replicate(&newProductID, &sourceVariations)
+	newAttributeGroups := product_attribute_group.Replicate(&newProductID, &sourceAttributeGroups)
+
+	attributeMap := map[string]*primitive.ObjectID{}
+	variationMap := map[string]*primitive.ObjectID{}
+	attributeGroupMap := map[string]*primitive.ObjectID{}
+
+	{
+		for _, item := range *newAttributes {
+			log.Println("newAttributes", item)
+			attributeMap[item.OriginalID.String()] = &item.ID
+		}
+
+		for _, item := range *newVariations {
+			variationMap[item.OriginalID.String()] = &item.ID
+		}
+
+		for _, item := range *newAttributeGroups {
+			attributeGroupMap[item.OriginalID.String()] = &item.ID
+		}
+	}
+
+	props := product_variations_attributes.Props_Replicate{
+		ProductId:       &newProductID,
+		SourceList:      &sourceVariationAttributes,
+		Attributes:      &attributeMap,
+		Variations:      &variationMap,
+		AttributeGroups: &attributeGroupMap,
+	}
+
+	newVariationAttributes := product_variations_attributes.Replicate(&props)
+
+	if len(*newVariationAttributes) != len(sourceVariationAttributes) {
+		t.Errorf("Replicate() = %v, want %v", len(*newVariationAttributes), len(sourceVariationAttributes))
+	}
+
 }
