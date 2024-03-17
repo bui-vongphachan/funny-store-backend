ad263268976d12242fe134601acfe433cc191a49
vongphachan~ <vongphachannukkalash@gmail.com>
2024-03-17 15:23:00 +0700

    Add token generation function

--- src/modules/authentication/service.go
+++ src/modules/authentication/service.go
@@ -1,8 +1,36 @@
 package authentication
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
 
-func ValidateToken() {}
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/vongphachan/funny-store-backend/src/modules/admins"
+	log_file "github.com/vongphachan/funny-store-backend/src/modules/log-file"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func ValidateToken(c *gin.Context) {
+
+}
+
+func GenerateToken(c *gin.Context, admin *admins.Admin) *string {
+	secretKey := []byte(KEY_Secret)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"data": admin,
+			"exp":  time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		log_file.SaveErrorLog(c, &err)
+		return nil
+	}
+
+	return &tokenString
+}
 
 func HashPassword(password *string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(*password), bcrypt.DefaultCost)
